b63547885b658a59d584ddcda621dd3c10321ec7
Adilet <mailadilet@gmail.com>
2018-10-12 07:04:44 +0600

    Completed functions in find_max.py and wrote unit tests for them

--- find_max.py
+++ find_max.py
@@ -1,29 +1,73 @@
 def get_max(a, b):
-    pass
+    """
+        return max number among a and b
+    """
+    if (a > b):
+        return a
+    else:
+        return b
 
 
 def get_max_without_arguments():
-    pass
+    """
+        raise TypeError exception with message
+    """
+    raise TypeError('no arguments')
 
 
 def get_max_with_one_argument(a):
-    pass
+    """
+        return that value
+    """
+    return a
 
 
 def get_max_with_many_arguments(*args):
-    pass
+    """
+        return biggest number among args
+    """
+    r = float('-inf')
+    for arg in args:
+        if (arg > r):
+            r = arg
+    return r
 
 
 def get_max_with_one_or_more_arguments(first, *args):
-    pass
+    """
+        return biggest number among first + args
+    """
+    r = first
+    for arg in args:
+        if (arg > r):
+            r = arg
+    return r
 
 
 def get_max_bounded(*args, low, high):
-    pass
+    """
+        return biggest number among args bounded by low & high
+    """
+    r = float('-inf')
+    for arg in args:
+        if (arg > r) and (low < arg < high):
+            r = arg
+    return r
 
 
 def make_max(*, low, high):
+    """
+        return inner function object which takes at last one argument
+        and returns biggest number among it's arguments, but if the
+        biggest number is bigger than the 'high' which given as required
+        argument the inner function has to return it.
+    """
+
     def inner(first, *args):
-        pass
+        r = float('-inf')
+        for arg in (first, ) + args:
+            if (arg > r) and (low < arg < high):
+                r = arg
+        return r
 
     return inner
