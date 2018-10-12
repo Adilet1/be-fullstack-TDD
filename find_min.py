8e659598db26ddb1fa29c053990f8a5206af9d23
Adilet <mailadilet@gmail.com>
2018-10-12 06:36:49 +0600

    Completed functions in find_min.py

--- find_min.py
+++ find_min.py
@@ -4,36 +4,57 @@ def get_min(a, b):
     """
         return min number among a and b
     """
+    if (a < b):
+        return a
+    else:
+        return b
 
 
 def get_min_without_arguments():
     """
         raise TypeError exception with message
     """
+    raise TypeError('no arguments')
 
 
 def get_min_with_one_argument(x):
     """
         return that value
     """
+    return x
 
 
 def get_min_with_many_arguments(*args):
     """
         return smallest number among args
     """
+    r = float('+inf')
+    for arg in args:
+        if (arg < r):
+            r = arg
+    return r
 
 
 def get_min_with_one_or_more_arguments(first, *args):
     """
         return smallest number among first + args
     """
+    r = first
+    for arg in args:
+        if (arg < r):
+            r = arg
+    return r
 
 
 def get_min_bounded(*args, low, high):
     """
         return smallest number among args bounded by low & high
     """
+    r = float('+inf')
+    for arg in args:
+        if (arg < r) and (low < arg < high):
+            r = arg
+    return r
 
 
 def make_min(*, low, high):
@@ -45,6 +66,10 @@ def make_min(*, low, high):
     """
 
     def inner(first, *args):
-        pass
+        r = float('+inf')
+        for arg in (first, ) + args:
+            if (arg < r) and (low < arg < high):
+                r = arg
+        return r
 
     return inner
